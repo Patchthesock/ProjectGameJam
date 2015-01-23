eee5fdef679129bb5c3247a2c91b72c18b9e0a1e
patchthesock <stephencallaghan91@gmail.com>
2015-01-23 18:44:31 +0000

    Added Photon Networking.

---
+++ Assets/Scripts/Networking/NetworkViewingManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewingNetworkManager : MonoBehaviour {
+
+	void Awake ()
+	{
+		if(!PhotonNetwork.connected)
+		{
+			// Connect using version number.
+			PhotonNetwork.ConnectUsingSettings("1");
+			PhotonNetwork.automaticallySyncScene = true;
+		}
+	}
+
+	void OnJoinedLobby ()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed ()
+	{
+		RoomOptions roomOptions = new RoomOptions() { isVisible = true, isOpen = true, 	maxPlayers = 3 };
+		PhotonNetwork.JoinOrCreateRoom( SystemInfo.deviceUniqueIdentifier, roomOptions, TypedLobby.Default); 
+	}
+
+	void OnJoinedRoom ()
+	{
+		Debug.Log ("I'm Online.");
+	}
+}
